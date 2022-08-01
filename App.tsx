70129712031d3ba31fd8336053383b495b431c48
Matt Calvert <matt@noon.studio>
2022-08-01 23:02:24 +0100

    styling mainly done

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, View, Platform, TextInput, TouchableOpacity } from 'react-native';
 import Task from './components/Task';
+
 
 export default function App() {
     return (
@@ -7,7 +8,7 @@ export default function App() {
             {/* Today's Tasks */}
             <View style={styles.tasksWrapper}>
                 <Text style={styles.sectionTile}>
-                    Today's Tasks?
+                    Today's Tasks
                 </Text>
 
                 <View style={styles.items}>
@@ -16,6 +17,21 @@ export default function App() {
                     <Task text={'Task 2'} />
                 </View>
             </View>
+
+            {/* Write a task section */}
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={styles.writeTaskWrapper}
+            >
+                <TextInput style={styles.input} placeholder={'Write a task'} />
+
+                <TouchableOpacity>
+                    <View style={styles.addWrapper}>
+                        <Text style={styles.addText}>+</Text>
+                    </View>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
+
         </View>
     );
 }
@@ -35,5 +51,36 @@ const styles = StyleSheet.create({
     },
     items: {
         marginTop: 30,
+    },
+    writeTaskWrapper: {
+        position: 'absolute',
+        bottom: 60,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    input: {
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        backgroundColor: '#ffffff',
+        borderRadius: 60,
+        borderColor: '#c0c0c0',
+        borderWidth: 1,
+        width: 250,
+
+    },
+    addWrapper: {
+        width: 60,
+        height: 60,
+        backgroundColor: '#ffffff',
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#c0c0c0',
+        borderWidth: 1,
+    },
+    addText: {
+
     }
 });
