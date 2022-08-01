b47055b655b224bc7e559a957861d555992d36a7
Matt Calvert <matt@noon.studio>
2022-08-01 22:15:15 +0100

    init commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            {/* Today's Tasks */}
+            <View style={styles.tasksWrapper}>
+                <Text style={styles.section}>
+                    Today's Tasks
+                </Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    tasksWrapper: {
+
+    },
+    section: {
+
+    }
+});
